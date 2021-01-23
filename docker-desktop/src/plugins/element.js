4cbad8d89c803abcc876a2d1605d343d3284fdb6
kusa-mochi <whoatemyapplepie@gmail.com>
2021-01-23 14:19:30 +0900

    fix layout on containers table.

--- docker-desktop/src/plugins/element.js
+++ docker-desktop/src/plugins/element.js
@@ -3,4 +3,6 @@ import Element from "element-ui";
 import "../element-variables.scss";
 import locale from "element-ui/lib/locale/lang/ja";
 
+import "../element-override.scss";
+
 Vue.use(Element, { locale });
