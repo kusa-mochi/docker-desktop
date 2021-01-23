a85a9c764ce3673f45a634e1a48c5bd9414fffcc
kusa-mochi <whoatemyapplepie@gmail.com>
2021-01-23 23:33:46 +0900

    update table layout on Images tab.

--- docker-desktop/src/components/Images.vue
+++ docker-desktop/src/components/Images.vue
@@ -1,20 +1,18 @@
 <template>
   <div id="images">
-    <el-table
-      :data="tableData"
-      height="500"
-    >
-    <el-table-column label="Repository" prop="repository"></el-table-column>
-    <el-table-column label="Tag" prop="tag"></el-table-column>
-    <el-table-column label="ID" prop="id"></el-table-column>
-    <el-table-column label="Size" prop="size"></el-table-column>
-    <el-table-column fixed="right" label="Operations" width="112">
+    <el-table :data="tableData" height="500">
+      <el-table-column label="Repository" prop="repository"></el-table-column>
+      <el-table-column label="Tag" prop="tag"></el-table-column>
+      <el-table-column label="ID" prop="id"></el-table-column>
+      <el-table-column label="Size" prop="size"></el-table-column>
+      <el-table-column fixed="right" label="Operations" width="168">
         <template>
           <el-button
             type="primary"
             icon="el-icon-caret-right"
             circle
           ></el-button>
+          <el-button type="secondary" icon="el-icon-upload2" circle></el-button>
           <el-button type="info" icon="el-icon-delete" circle></el-button>
         </template>
       </el-table-column>
